9f090856d7fe718f7af988ed4735344bc7911aec
tkztmk <tkztmk@outlook.com>
2012-08-16 05:46:32 +0900

    Add git-completion.bash loading to default-profile.sh

--- script/bash/default-profile.sh
+++ script/bash/default-profile.sh
@@ -2,6 +2,7 @@
 export EDITOR=vim
 export TERM=xterm-256color
 export LANG=en_US.utf8
+. /usr/share/doc/git-*/contrib/completion/git-completion.bash
 eval "$(rbenv init -)"
 . ~/local/add-path.sh
 hash -r
