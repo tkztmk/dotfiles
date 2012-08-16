de94027f67f9061dcd8cd1b5a5baf3ff4d7021d1
tkztmk <tkztmk@outlook.com>
2012-08-17 02:58:18 +0900

    Fix bugs in script/bash/default-profile.sh and add some comments.

--- script/bash/default-profile.sh
+++ script/bash/default-profile.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
+# set some Environment Variable. 
 export EDITOR=vim
 export TERM=xterm-256color
 export LANG=en_US.utf8
+
+# find and load git-completion.bash
 for s in $(find $(find /usr/share/doc -maxdepth 1 -name git-*) /usr/share/git \
   -name git-completion.bash); do
   . $s
 done
 
-
-eval "$(rbenv init -)"
+# add to $PATH and rehash. 
 . ~/local/add-path.sh
 hash -r
+
+# initialize rbenv. 
+eval "$(rbenv init -)"
 
